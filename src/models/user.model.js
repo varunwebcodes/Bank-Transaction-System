f69ab119c96eae2a70a4da682499cd9fae4c5b4a
Varun Negi <varunnegiweb@gmail.com>
2026-02-20 17:04:59 +0530

    Authentication routes , Controllers implementation , JWT and Cookie-Parser Setup

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -28,13 +28,13 @@ const userSchema = new mongoose.Schema({
 userSchema.pre("save", async function(next){
 
     if(!this.isModified("password")){
-        return next()
+        return 
     }
 
     const hash = await bcrypt.hash(this.password, 10)
     this.password = hash
 
-    return next()
+    return
 })
 
 userSchema.methods.comparePassword = async function(password){
